3bda7996ed7894e653eda756931e603f1643bf24
VaneRaklan <antoniowii@yahoo.com>
2015-11-19 10:58:20 -0800

    Created the SOTABots Teleop mode and registered it so it can be selected on the driver station.

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SOTABotsTeleop.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by Antonio on 11/19/2015.
+ */
+public class SOTABotsTeleop {
+    public void init(){
+
+    }
+    public void loop(){
+
+    }
+}
